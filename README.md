3f48456e0498bef98fd62021ba681f8a419f2f49
Rodzers <rodzers.u@gmail.com>
2024-09-14 23:23:21 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# nba-app
+Personal Project
---
+++ src/main/java/com/example/nba_app_backend/NbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nba_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaApplication.class, args);
+	}
+
+}
